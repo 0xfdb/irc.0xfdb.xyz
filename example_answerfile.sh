6ec13860db4f10aff14c5ea81d6818ad4a1c08d6
f0ur0ne <admin@0xfdb.xyz>
2020-06-05 20:35:27 -0400

    Create example_answerfile.sh

---
+++ example_answerfile.sh
@@ -0,0 +1,19 @@
+#1/bin/bash
+#Answer file for unrealircd_0xfdb.sh
+
+#Server Name:
+XFDBSRVNAME=""
+#Server ID:
+XFDBSRVID=""
+#Owner Name:
+XFDBOPFNAME=""
+#Owner Nick:
+XFDBOPNAME=""
+#Owner Email:
+$XFDBOPEMAIL=""
+#Server Op:
+XFDBOPNICK=""
+#Server Op password:
+XFDBOPPASSWD=""
+#Server Op vhost:
+XFDBOPVHOST=""
